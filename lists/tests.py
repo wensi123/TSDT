f7498d60867780e3b9c950638b5e587d51cdd336
wensi123 <1763506289@qq.com>
2025-05-07 17:31:12 +0800

    message 10: First model migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,29 @@
+# tests.py
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+from lists.models import Item # This will initially cause an error
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
 
 class HomePageTest(TestCase):
 
